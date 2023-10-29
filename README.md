9d7a1f6b62f45f8712436cfb3fb2bdc88b574585
Andre Luiz <andreluiz.jpofi@gmail.com>
2023-10-28 22:10:04 -0300

    removing all

--- README.md
+++
@@ -1,2 +0,0 @@
-# label-generate
-Generate Label for print
--- src/model/exceptions/FormatException.java
+++
@@ -1,9 +0,0 @@
-package model.exceptions;
-
-public class FormatException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-
-	public FormatException(String msg) {
-		super(msg);
-	}
-}
