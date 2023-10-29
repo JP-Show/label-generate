9f2c014e0185eb5e073b9e898da3f68680ce013b
Andre Luiz <andreluiz.jpofi@gmail.com>
2023-10-28 22:25:02 -0300

    create my Exception class

---
+++ src/main/java/com/exceptions/myException.java
@@ -0,0 +1,10 @@
+package com.exceptions;
+
+public class myException extends Exception {
+  public myException (String msg){
+    super(msg);
+  }
+  public myException (String msg, Throwable cause){
+    super(msg, cause);
+  }  
+}
