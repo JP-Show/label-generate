04c94dfa1503fcd18809f42fa117c6d6c8d4c532
Andre Luiz <andreluiz.jpofi@gmail.com>
2023-10-29 15:08:33 -0300

    create class DOS and method exec

---
+++ src/main/java/com/utils/DOS.java
@@ -0,0 +1,34 @@
+package com.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class DOS{
+  public static List<String> exec (String [] command) throws IOException{
+    List<String> list = new ArrayList<>();
+    try {
+      ProcessBuilder pb = new ProcessBuilder(command);
+      Process process = pb.start();
+    
+      InputStream is = process.getInputStream();
+      InputStreamReader isr = new InputStreamReader(is);
+      BufferedReader br = new BufferedReader(isr);
+
+      Object[] listObjects = br.lines().toArray();
+      
+      for(Object item : listObjects){
+        if(!item.toString().equals("")){
+          list.add((String) item);
+        }
+      }
+    } catch (Exception e) {
+      throw new IOException("Error ao executar o DOS - " + e.getMessage());
+    }
+    return list;
+  } 
+}
