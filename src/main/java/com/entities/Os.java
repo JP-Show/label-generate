b2048bb71722f3c357ea6eb4f0e5b37a73296d20
Andre Luiz <andreluiz.jpofi@gmail.com>
2023-10-29 19:14:14 -0300

    Create Os class and implement

---
+++ src/main/java/com/entities/Os.java
@@ -0,0 +1,34 @@
+package com.entities;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.exceptions.myException;
+import com.utils.DOS;
+import com.utils.Sysinfo;
+import com.utils.Regx;
+
+public class Os {
+  
+  public static String getName() throws myException, IOException{
+    String name = "unknown";
+    int version = 00;
+    
+    try{
+      version = Sysinfo.info().getOperatingSystem().getBitness();
+
+      List<String> list = DOS.exec(new String[] {"cmd.exe", "/c", "systeminfo | findstr /C:\"Nome do sistema operacional\""});
+      String nameOS = list.get(0);
+
+      name = Regx.cutString(".*(Windows \\d+\\s?\\w*)", nameOS);
+    }
+    catch (myException e){
+      throw new myException("Error ao pegar o nome do sistema operacional - " + e.getMessage(), e.getCause());
+    }
+    catch (Exception e) {
+      throw new myException("Error ao pegar o nome do sistema operacional - " + e.getMessage(), e.getCause());
+    }
+    String Os = name + " x" + version;
+    return Os;
+  }
+}
