159ce906ab039b0c20371cb0ec7bf8bafb73f2f4
Andre Luiz <andreluiz.jpofi@gmail.com>
2023-10-29 19:13:50 -0300

    create a regex class and implement cutString

---
+++ src/main/java/com/utils/Regx.java
@@ -0,0 +1,26 @@
+package com.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.exceptions.myException;
+
+public class Regx {
+  
+  public static String cutString (String regex, String text) throws Exception{
+    String result = "unknown";
+    try{
+      Pattern patt = Pattern.compile(regex);
+
+      Matcher match = patt.matcher(text);
+
+      if(match.matches()){
+        result = match.group(1);
+      }
+    }catch (Exception e) {
+      throw new myException("Error ao tentar capturar um texto específico" + e.getMessage(), e.getCause());
+    }
+  return result;
+
+  }
+}
