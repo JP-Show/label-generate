005bb2f8111afe71b00dd2f3de10e7504b80746d
Andre Luiz <andreluiz.jpofi@gmail.com>
2023-10-29 01:13:27 -0300

    "implement method getName in Cpu"

--- src/main/java/com/entities/cpu.java
+++ src/main/java/com/entities/cpu.java
@@ -1,23 +1,38 @@
 package com.entities;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-public class cpu {
-  public void getName (){
-    .getHardware().getProcessor().getProcessorIdentifier();
-    if(si.getModel() == "uknown"){
-      ProcessBuilder pb = new ProcessBuilder("wmic", "cpu", "get", "name");
-      Process process = pb.start();
+import com.exceptions.myException;
+import com.utils.Sysinfo;
 
-      InputStream is = process.getInputStream();
-      InputStreamReader isr = new InputStreamReader(is);
-      BufferedReader br = new BufferedReader(isr);
+import oshi.hardware.CentralProcessor.ProcessorIdentifier;
 
-      String[] line = (String[]) br.lines().toArray();
-      
+public class Cpu {
+  public static String getName (){
+    String cpu = "unknown";
+    try {
+      ProcessorIdentifier pi = Sysinfo.info().getHardware().getProcessor().getProcessorIdentifier();
+      if(pi.getModel() == "unknown"){
+        ProcessBuilder pb = new ProcessBuilder("wmic", "cpu", "get", "name");
+        Process process = pb.start();
 
+        InputStream is = process.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+
+        String[] line = (String[]) br.lines().toArray();
+        cpu = line[0];
+      }else{
+       cpu = pi.getName();
+      }
+    }catch (myException e) {
+      System.out.println("error ao pegar nome da CPU - " + e.getCause());
+    }catch(IOException e){
+      System.out.println("error ao pegar nome da CPU - " + e.getCause());
     }
+    return cpu;
   }
 }
--- src/main/java/com/utils/Sysinfo.java
+++ src/main/java/com/utils/Sysinfo.java
@@ -1,10 +1,12 @@
 package com.utils;
 
+import com.exceptions.myException;
+
 import oshi.SystemInfo;
 
 public class Sysinfo {
 
-  public static SystemInfo info (){
+  public static SystemInfo info () throws myException {
     SystemInfo si = new SystemInfo();
     return si;
   }
