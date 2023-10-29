ffd815e0a5348e58ab1962972c62681a0d3fe247
Andre Luiz <andreluiz.jpofi@gmail.com>
2023-10-29 15:15:38 -0300

    implement motherboard

--- src/main/java/com/entities/motherboard.java
+++ src/main/java/com/entities/motherboard.java
@@ -1,7 +1,36 @@
 package com.entities;
 
+import java.io.IOException;
+import java.util.List;
+
+import com.exceptions.myException;
+import com.utils.DOS;
+import com.utils.Sysinfo;
+
 import oshi.hardware.Baseboard;
 
-public class motherboard {
-  Baseboard bb;
+public class Motherboard {
+
+  public static String getName()throws myException, IOException {
+    String mb = "unknown";
+    try{
+      Baseboard bb = Sysinfo.info().getHardware().getComputerSystem().getBaseboard();
+      if(bb.getModel() == "unknown"){
+        List<String> resultDOS = DOS.exec(new String[] {"wmic", "baseboard", "get", "product"});
+        int lastIndex = resultDOS.size() - 1;
+        mb = resultDOS.get(lastIndex);
+      }else{
+        System.out.println(bb);
+        mb = bb.getModel();
+      }
+    }catch(myException e){
+      throw new myException("Error ao pegar o nome da placa mãe - " + e.getMessage());
+    }
+    catch(IOException e){
+      throw new IOException("Error ao pegar o nome da placa mãe - " + e.getMessage());
+    }
+    
+    return mb;
+  }
+
 }
