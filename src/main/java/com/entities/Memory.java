002985c4e0054de301f580f632270464f252212f
Andre Luiz <andreluiz.jpofi@gmail.com>
2023-10-30 15:14:17 -0300

    implement Memory

--- src/main/java/com/entities/Memory.java
+++ src/main/java/com/entities/Memory.java
@@ -9,11 +9,11 @@ import com.utils.Sysinfo;
 import oshi.hardware.PhysicalMemory;
 
 public class Memory {
-  private static List<Integer> getMemoryList ()throws myException{
+  private static List<Integer> getMemoryList() throws myException {
     List<Integer> listMemory = new ArrayList<>();
     try {
       List<PhysicalMemory> listPH = Sysinfo.info().getHardware().getMemory().getPhysicalMemory();
-      for(PhysicalMemory memory : listPH){
+      for (PhysicalMemory memory : listPH) {
         double cap = memory.getCapacity() * 0.000000001;
         int mem = (int) Math.floor(cap);
         listMemory.add(mem);
@@ -25,51 +25,64 @@ public class Memory {
     return listMemory;
   }
 
-  private List<Ram> addRamToList (List<Integer>listMemory){
+  private static List<Ram> addRamToList(List<Integer> listMemory) throws myException {
     List<Ram> listRam = new ArrayList<>();
-    for(int mem : listMemory){
-      int staticSize = listRam.size();
-      if(listRam.size() == 0){
-        Ram ram = new Ram(1, mem);
-        listRam.add(ram);
-      }
-      if(staticSize > 0){
-        for(int i = 1; i <= staticSize; i++ ){
-          if(listRam.get(i - 1).ram == mem){
-            listRam.get(i- 1).setQnt(1);
-            break;
+    try {
+      for (int mem : listMemory) {
+        int staticSize = listRam.size();
+        if (listRam.size() == 0) {
+          Ram ram = new Ram(1, mem);
+          listRam.add(ram);
+        }
+        if (staticSize > 0) {
+          for (int i = 1; i <= staticSize; i++) {
+            if (listRam.get(i - 1).ram == mem) {
+              listRam.get(i - 1).setQnt(1);
+              break;
+            }
+            if (i == staticSize) {
+              Ram ram = new Ram(1, mem);
+              listRam.add(ram);
+            }
           }
-          if(i == staticSize ){
-            Ram ram = new Ram(1, mem);
-            listRam.add(ram);
-          } 
         }
       }
+    } catch (Exception e) {
+      throw new myException("Erro ao adicionar a memória na lista - " + e.getMessage(), e.getCause());
+
     }
+
     return listRam;
   }
 
-  private List<Ram> buildRam() throws myException{
+  private static List<Ram> buildRam() throws myException {
+
     List<Integer> listMemory = getMemoryList();
     List<Ram> listRam = addRamToList(listMemory);
     return listRam;
   }
 
-  public String getName () throws myException{
+  public static String getName() throws myException {
     List<Ram> listRam = buildRam();
-    StringBuilder sb = new StringBuilder("");
+    StringBuilder sb = new StringBuilder();
 
-    for(Ram ram : listRam){
-    sb.append(ram.qnt)
-    } 
+    for (Ram ram : listRam) {
+      if (ram.equals(listRam.get(listRam.size() - 1))) {
+        sb.append(ram.qnt + "x" + ram.ram + "Gb");
+        break;
+      }
+      sb.append(ram.qnt + "x" + ram.ram + "Gb |");
+    }
+    return sb.toString();
   }
 }
 
-class Ram { 
+class Ram {
   int qnt = 1;
   int ram;
 
-  public Ram() {}
+  public Ram() {
+  }
 
   public Ram(int qnt, int ram) {
     this.qnt = qnt;
@@ -79,15 +92,16 @@ class Ram {
   public int getQnt() {
     return qnt;
   }
+
   public void setQnt(int qnt) {
     this.qnt += qnt;
   }
+
   public int getRam() {
     return ram;
   }
+
   public void setRam(int ram) {
     this.ram = ram;
   }
-
-  
 }
